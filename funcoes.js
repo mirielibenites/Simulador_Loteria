ca054a6e4f52e455fe24fc628d565c3d53948cf0
mirielibenites <mirielibenites@gmail.com>
2022-03-08 17:24:58 -0400

    atualização do codigo

--- funcoes.js
+++ funcoes.js
@@ -1,4 +1,5 @@
 var numSort = []
+var numDig = []
 
 function sorteioNum(){
     numSort = []
@@ -6,8 +7,35 @@ function sorteioNum(){
         let sort = Math.ceil(Math.random()*60)
         if(!numSort.includes(sort))
         numSort.push(sort)
-    } while (numSort.length<6);
+    } while (numSort.length<6)
+
+    document.getElementById("num-sort").innerHTML = numSort
     
     console.log(numSort)
 
+    verificaAcertos()
+
+}
+
+function getNum(valor, pos){
+    valor = Number(valor)
+    if(valor>60){
+        alert("O valor é maior que 60.")
+    }else if(valor<=0){
+        alert("O valor não pode ser menor que 0.")
+    }else if(numDig.includes(valor)){
+        alert("O valor já foi digitado.")
+    }else{
+        numDig[pos] = valor
+    }
+}
+
+function verificaAcertos(){
+    let cont=0
+    numDig.forEach(function(valor, index){
+        if(numSort.includes(valor)){
+            cont = cont + 1
+        }
+    })
+    document.getElementById("total").innerText = cont
 }
